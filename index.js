f71553bb7f6064e6d8050d5bb60bb6eb1e3399b0
Kirah Sapong <kirah@tbd.email>
2024-01-04 17:36:17 -0800

    Add prompts

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import path from 'path';
 import fs from 'fs-extra';
 import minimist from 'minimist';
+import prompts from 'prompts';
 import { fileURLToPath } from 'url';
 import { createRequire } from 'node:module';
 
@@ -11,8 +12,62 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const require = createRequire(import.meta.url);
 
+if (argv.t && argv.template) {
+  console.error(`Error: illegal options: specify either -t or --template, not both.`)
+  process.exit(1)
+}
+if (argv.s && argv.sync) {
+  console.error(`Error: illegal options: specify either -s or --sync, not both.`)
+  process.exit(1)
+}
+if (argv.e && argv.endpoints) {
+  console.error(`Error: illegal options: specify either -e or --endpoint, not both.`)
+  process.exit(1)
+}
+
+let template = argv.t || argv.template;
+let sync = argv.s || argv.sync;
+let endpoints = argv.e?.join(', ') || argv.endpoints;
+
 async function init() {
-  const targetDir = argv._[0] || '.' // last arg ie. npx create web5/@latest my-app
+  if (!template || !sync || !endpoints) {
+    await (async () => {
+      let options = [];
+      if (!template) {
+        options.push({
+          type: 'select',
+          name: 'template',
+          message: 'Choose a template:',
+          choices: [
+            { title: 'Vanilla JS', value: 'vanilla' }
+          ],
+        })
+      }
+      if (!sync) {
+        options.push({
+          type: 'text',
+          name: 'sync',
+          message: 'Set your DWN sync interval, eg. 2s; 2m; 2h; 2d; (Default set if not provided: 2m)',
+          initial: '',
+          validate: sync => /^[0-9][a-z]/.test(sync) || sync === '' ? true : 'Please enter response as a valid time format eg. 2s; 2m; 2h; 2d'
+        })
+      }
+      if (!endpoints) {
+        options.push({
+          type: 'text',
+          name: 'endpoints',
+          message: 'Set DWN endpoints (Default set if not provided)',
+          initial: '',
+          validate: endpoints => /^https?:\/\//.test(endpoints) || endpoints === '' ? true : 'Please enter a valid URL'
+        })
+      }
+      const response = await prompts(options, { onCancel: () => { console.error(`Project terminated.`); process.exit(1) }});
+      template = template || response.template;
+      sync = sync || response.sync;
+      endpoints = endpoints || response.endpoints;
+    })();
+  }
+  const targetDir = argv._[0] || '.' // last arg ie. `my-app` in `npx create web5/@latest my-app`
   const cwd = process.cwd()
   const root = path.join(cwd, targetDir)
   const renameFiles = {
@@ -29,7 +84,7 @@ async function init() {
 
   const templateDir = path.join(
     __dirname,
-    `template-${argv.t || argv.template || 'vanilla'}` // -t or --template=example-template
+    `template-${template || 'vanilla'}` // -t or --template=template-vanilla
   )
  
   const write = async (file, content) => {
@@ -48,23 +103,15 @@ async function init() {
       const pkg = require(path.join(templateDir, `package.json`))
       pkg.name = path.basename(root)
       await write(file, JSON.stringify(pkg, null, 2))
-    } else if (file === 'web5-utils.js' && (argv.s || argv.sync || argv.e || argv.endpoint)) {
-      if (argv.s && argv.sync) {
-        console.error(`Error: illegal options: specify either -s or --sync, not both.`)
-        process.exit(1)
-      }
-      if (argv.e && argv.endpoint) {
-        console.error(`Error: illegal options: specify either -e or --endpoint, not both.`)
-        process.exit(1)
-      }
+    } else if (file === 'web5-utils.js' && (sync || endpoints)) {
       fs.readFile(path.join(templateDir, `web5-utils.js`), 'utf8', async function (err, data) {
         if (err) {
           return console.log(err);
         }
-        const sync = (argv.s || argv.sync) ? `sync: '${argv.s || argv.sync}'` : undefined // -s or --sync=2m
-        const dwnEndpoints = (argv.e || argv.endpoint) ? `dwnEndpoints: ['${argv.e?.join(', ') || argv.endpoint}']` : undefined // -e for each endpoint or --endpoint=test.com
-        const concat = sync && dwnEndpoints ? sync + ', ' + dwnEndpoints : undefined
-        const result = data.replace(/Web5.connect\(\)/g, `Web5.connect({ ${concat ?? sync ?? dwnEndpoints } })`);
+        const dwnSync = (sync) ? `sync: '${sync}'` : undefined // -s or --sync=2m
+        const dwnEndpoints = (endpoints) ? `dwnEndpoints: ['${endpoints}']` : undefined // -e for each endpoint or --endpoints=https://test.com,https://test2.com
+        const concat = dwnSync && dwnEndpoints ? dwnSync + ', ' + dwnEndpoints : undefined
+        const result = data.replace(/Web5.connect\(\)/g, `Web5.connect({ ${concat ?? dwnSync ?? dwnEndpoints } })`);
         await write(file, result)
       });
     } else {
@@ -76,8 +123,8 @@ async function init() {
   if (root !== cwd) {
     console.log(`  cd ${path.relative(cwd, root)}`)
   }
-  console.log(`npm install`)
-  console.log(`npm run serve`)
+  console.log(`  npm install`)
+  console.log(`  npm run serve`)
   console.log()
 }
 
