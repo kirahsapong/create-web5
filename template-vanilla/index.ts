446764440bf1d85ec042c76255275ac37dc845c3
Kirah Sapong <kirah@tbd.email>
2024-01-04 18:53:14 -0800

    import style

--- template-vanilla/index.ts
+++ template-vanilla/index.ts
@@ -1,4 +1,5 @@
 import { HelloWeb5, handleStateOnLoad } from './components/HelloWeb5.js';
+import './style.css'
 
 document.querySelector('#app')!.innerHTML = HelloWeb5();
 await handleStateOnLoad();
